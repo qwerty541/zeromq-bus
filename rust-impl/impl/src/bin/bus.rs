2b2c60dae081649a59827ecedfc647ccc69c41b2
Alexandr <qwerty541zxc@gmail.com>
2021-06-30 17:08:40 +0300

    rust-impl: split BUS into receiving and sending parts for optimization

--- rust-impl/impl/src/bin/bus.rs
+++ rust-impl/impl/src/bin/bus.rs
@@ -35,6 +35,8 @@ use std::collections::VecDeque;
 use std::convert::From;
 use std::env;
 use std::iter::Iterator;
+use std::sync::mpsc;
+use std::thread;
 use std::time::Duration;
 use std::time::Instant;
 use std::time::SystemTime;
@@ -107,73 +109,86 @@ fn main() {
     );
 
     let mut total_processed_messages_count: usize = 0;
+    let (received_messages_channel_sender, received_messages_channel_receiver) =
+        mpsc::channel::<Vec<u8>>();
 
-    log::debug!("running messages processing loop");
+    log::debug!("running sender thread");
+    drop(thread::spawn(move || {
+        #[allow(unused_labels)]
+        'messages_sender: loop {
+            let message_bytes = errored_messages_bytes_buffer.pop_front().map_or_else(
+                || {
+                    received_messages_channel_receiver
+                        .recv()
+                        .expect("received messages mpsc sender dropped")
+                },
+                |errored_message_bytes| errored_message_bytes,
+            );
 
-    'messages_processing: loop {
-        let message_bytes =
-            if let Some(errored_message_bytes) = errored_messages_bytes_buffer.pop_front() {
-                errored_message_bytes
-            } else {
-                // Firstly receive first message frame which is the sender identity.
-                let identity_bytes = match router_socket.recv_bytes(ZEROMQ_ZERO_FLAG) {
-                    Ok(identity_bytes) => identity_bytes,
-                    Err(error) => {
-                        log::error!("failed to receive sender identity because of: {}", error);
-                        continue 'messages_processing;
-                    }
-                };
+            let mut index_of_publisher_that_will_be_used = 0;
+            let mut max_duration_since_last_action = Duration::from_nanos(0_u64);
 
-                log::trace!("< [IDENTITY] {:?}", identity_bytes);
+            for (index, publisher) in publishers.iter().enumerate() {
+                let current_duration_since_last_action =
+                    publisher.get_last_action_time().duration_since(*INIT_TIME);
+                if current_duration_since_last_action > max_duration_since_last_action {
+                    max_duration_since_last_action = current_duration_since_last_action;
+                    index_of_publisher_that_will_be_used = index;
+                }
+            }
 
-                // In case if we succeed to receive identity try to receive next frame which
-                // include message content.
-                let message_bytes = match router_socket.recv_bytes(ZEROMQ_ZERO_FLAG) {
-                    Ok(message_bytes) => message_bytes,
-                    Err(error) => {
-                        log::error!("failed to receive message because of: {}", &error);
-                        continue 'messages_processing;
-                    }
-                };
+            match (*publishers[index_of_publisher_that_will_be_used])
+                .send(Message::from(message_bytes.clone()), ZEROMQ_ZERO_FLAG)
+            {
+                Ok(()) => {
+                    log::trace!("> {:?}", message_bytes);
+                    total_processed_messages_count += 1;
+                }
+                Err(error) => {
+                    log::error!("failed to send message because of: {}", error);
+                    errored_messages_bytes_buffer.push_back(message_bytes);
+                }
+            }
 
-                message_bytes
-            };
+            publishers[index_of_publisher_that_will_be_used].update_last_action_time();
+
+            if total_processed_messages_count % REQUESTS_COUNT_INSIDE_ONE_GROUP == 0 {
+                log::debug!(
+                    "{:?} | total processed {} messages",
+                    SystemTime::now(),
+                    total_processed_messages_count
+                );
+            }
+        }
+    }));
+
+    log::debug!("running received loop");
+    'messages_receiver: loop {
+        // Firstly receive first message frame which is the sender identity.
+        let identity_bytes = match router_socket.recv_bytes(ZEROMQ_ZERO_FLAG) {
+            Ok(identity_bytes) => identity_bytes,
+            Err(error) => {
+                log::error!("failed to receive sender identity because of: {}", error);
+                continue 'messages_receiver;
+            }
+        };
+
+        log::trace!("< [IDENTITY] {:?}", identity_bytes);
+
+        // In case if we succeed to receive identity try to receive next frame which
+        // include message content.
+        let message_bytes = match router_socket.recv_bytes(ZEROMQ_ZERO_FLAG) {
+            Ok(message_bytes) => message_bytes,
+            Err(error) => {
+                log::error!("failed to receive message because of: {}", &error);
+                continue 'messages_receiver;
+            }
+        };
 
         log::trace!("< {:?}", message_bytes);
 
-        let mut index_of_publisher_that_will_be_used = 0;
-        let mut max_duration_since_last_action = Duration::from_nanos(0_u64);
-
-        for (index, publisher) in publishers.iter().enumerate() {
-            let current_duration_since_last_action =
-                publisher.get_last_action_time().duration_since(*INIT_TIME);
-            if current_duration_since_last_action > max_duration_since_last_action {
-                max_duration_since_last_action = current_duration_since_last_action;
-                index_of_publisher_that_will_be_used = index;
-            }
-        }
-
-        match (*publishers[index_of_publisher_that_will_be_used])
-            .send(Message::from(message_bytes.clone()), ZEROMQ_ZERO_FLAG)
-        {
-            Ok(()) => {
-                log::trace!("> {:?}", message_bytes);
-                total_processed_messages_count += 1;
-            }
-            Err(error) => {
-                log::error!("failed to send message because of: {}", error);
-                errored_messages_bytes_buffer.push_back(message_bytes);
-            }
-        }
-
-        publishers[index_of_publisher_that_will_be_used].update_last_action_time();
-
-        if total_processed_messages_count % REQUESTS_COUNT_INSIDE_ONE_GROUP == 0 {
-            log::debug!(
-                "{:?} | total processed {} messages",
-                SystemTime::now(),
-                total_processed_messages_count
-            );
-        }
+        received_messages_channel_sender
+            .send(message_bytes)
+            .expect("received messages mpsc receiver dropped");
     }
 }
